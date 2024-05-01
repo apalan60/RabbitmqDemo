69ae6a5b7a5d1a75e78da3c0f902d6cb3d773b0b
apalan60 <apalan60@gmail.com>
2024-05-01 10:54:14 +0800

    feat: implement basic publisher-cosumer pattern in console app.

---
+++ Receive/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+// declare the queue from which we're going to consume
+channel.QueueDeclare(
+    queue: "queue.consoleApp.Client1",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+//Declare a consumer
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (_, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+};
+
+//Consume the message
+channel.BasicConsume(queue: "queue.consoleApp.Client1",
+                     autoAck: true, 
+                     consumer: consumer);
+                     
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
