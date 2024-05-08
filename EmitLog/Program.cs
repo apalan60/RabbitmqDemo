c7d6760982a0cd13dccdf0cbd34e45082673fa96
apalan60 <apalan60@gmail.com>
2024-05-09 07:20:58 +0800

    Publish/Scribe pattern

---
+++ EmitLog/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+//The messages will be lost if no queue is bound to the exchange yet, but that's okay for us; if no consumer is listening yet we can safely discard the message.
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+var message = GetMessage(args);
+var body = Encoding.UTF8.GetBytes(message);
+
+
+channel.BasicPublish(exchange: "logs", routingKey: string.Empty, basicProperties: null, body: body);
+
+
+
+
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+static string GetMessage(string[] args)
+{
+    return args.Length > 0 ? string.Join(" ", args) : "info: Hello World!";
+}
---
+++ ReceiveLogs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+//Binding the exchange to the queue
+var queueName = channel.QueueDeclare().QueueName;   //a random queue name. For example it may look like amq.gen-JzTY20BRgKO-HjmUJj0wLg.
+channel.QueueBind(queue: queueName, exchange: "logs", routingKey: string.Empty);
+
+Console.WriteLine(" [*] Waiting for logs.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (_, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] {message}");
+};
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);  
+
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
