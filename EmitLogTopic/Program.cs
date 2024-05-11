4146c81e71991b5039d4fb48f300dabb9ba3d06a
apalan60 <apalan60@gmail.com>
2024-05-11 12:39:28 +0800

    Topic Exchange

---
+++ EmitLogTopic/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+const string exchangeName = "topic_logs";
+var routingKey = args.Length > 0 ? args[0] : "anonymous.info";
+var message = args.Length > 1 ? string.Join(" ", args.Skip(1).ToArray()) : "Hello World!";
+
+channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Topic);
+
+channel.BasicPublish(exchange: exchangeName,
+    routingKey: routingKey,
+    basicProperties: null,
+    body:Encoding.UTF8.GetBytes(message));
+
+Console.WriteLine($" [x] Sent '{routingKey}':'{message}'");
---
+++ ReceiveLogsTopic/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory{HostName = "localhost"};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+const string exchangeName = "topic_logs";
+channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Topic);
+
+//Create binding keys
+var queueName = channel.QueueDeclare().QueueName;
+
+if (ArgsIsEmpty(args)) return;
+foreach (var bindingKey in args)
+{
+    channel.QueueBind(queue: queueName, exchange: exchangeName, routingKey: bindingKey);
+}
+
+Console.WriteLine(" [*] Waiting for logs.");
+
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    var routingKey = ea.RoutingKey;
+    Console.WriteLine(" [x] Received '{0}':'{1}'", routingKey, message);
+};
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+
+
+
+
+
+bool ArgsIsEmpty(IReadOnlyCollection<string> strings)
+{
+    if (strings.Count >= 1) return false;
+    Console.Error.WriteLine("Usage: {0} [binding_key...]",
+        Environment.GetCommandLineArgs()[0]);
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+    Environment.ExitCode = 1;
+    return true;
+}
