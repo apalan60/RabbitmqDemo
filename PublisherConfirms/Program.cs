270b01dd7806475963b66bead6531841691090d6
apalan60 <apalan60@gmail.com>
2024-05-25 17:32:37 +0800

    HandlePublishConfirmsAsynchronously

--- PublisherConfirms/Program.cs
+++ PublisherConfirms/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
 using System.Text;
 using RabbitMQ.Client;
 
@@ -6,6 +7,7 @@ const int messageCount = 50000;
 
 PublishMessagesIndividually();
 PublishMessagesInBatch();
+await HandlePublishConfirmsAsynchronously();
 
 static void PublishMessagesIndividually()
 {
@@ -63,6 +65,72 @@ static void PublishMessagesInBatch()
     Console.WriteLine($"Published {messageCount:N0} messages in batch of {batchSize} in {Stopwatch.GetElapsedTime(startTime, endTime).TotalMilliseconds:N0} ms");
 }
 
+static async Task HandlePublishConfirmsAsynchronously()
+{
+    using var connection = CreateConnection();
+    using var channel = connection.CreateModel();
+    
+    var queueName = channel.QueueDeclare().QueueName;
+    channel.ConfirmSelect();
+    
+    var outstandingConfirms = new ConcurrentDictionary<ulong, string>();
+    
+    var startTime = Stopwatch.GetTimestamp();
+    for (int i = 0; i < messageCount; i++)
+    {
+        var body = Encoding.UTF8.GetBytes(i.ToString());
+        outstandingConfirms.TryAdd(channel.NextPublishSeqNo, i.ToString());
+        channel.BasicPublish(string.Empty, queueName, null, body);
+    }
+    
+    //2 callbacks: one for confirmed messages and one for nack-ed messages (messages that can be considered lost by the broker)
+    channel.BasicAcks += (_, args) =>  CleanOutstandingConfirms(args.DeliveryTag, args.Multiple); 
+    
+    channel.BasicNacks += (_, args) =>
+    {
+        outstandingConfirms.TryGetValue(args.DeliveryTag, out var body);
+        Console.WriteLine($"Message with body {body} has been nack-ed. Sequence number: {args.DeliveryTag}, multiple: {args.Multiple}");
+        CleanOutstandingConfirms(args.DeliveryTag, args.Multiple);
+    };
+    
+    if (!await WaitUntil(60, () => outstandingConfirms.IsEmpty))
+        throw new Exception("All messages could not be confirmed in 60 seconds");
+    
+    var endTime = Stopwatch.GetTimestamp();
+    Console.WriteLine($"Published {messageCount:N0} messages asynchronously in {Stopwatch.GetElapsedTime(startTime, endTime).TotalMilliseconds:N0} ms");
+    
+    
+    //持續檢查 condition 是否為真，並在每次檢查之間等待 100 豪秒。如果在指定的時間內條件變為真，則方法返回 true；如果時間到期而條件仍未變為真，則返回 false。
+    async Task<bool> WaitUntil(int seconds, Func<bool> condition)
+    {
+        int waited = 0;
+        while(!condition() && waited < seconds * 1000)
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(100));
+            waited += 100;
+        }
+        
+        return condition();
+    }
+    
+    void CleanOutstandingConfirms(ulong deliveryTag, bool multiple)
+    {
+        if (multiple)  //If false, only one message is confirmed/nack-ed, if true, all messages with a lower or equal sequence number are confirmed/nack-ed
+        {
+            var confirmedSeqNos = outstandingConfirms.Keys.Where(seqNo => seqNo <= deliveryTag);
+            foreach (var seqNo in confirmedSeqNos)
+            {
+                outstandingConfirms.TryRemove(seqNo, out _);
+            }
+        }
+        else
+        {
+            outstandingConfirms.TryRemove(deliveryTag, out _);
+        }
+    }
+}
+
+
 
 static IConnection CreateConnection()
 {
@@ -73,3 +141,4 @@ static IConnection CreateConnection()
 
     return factory.CreateConnection();
 }
+
