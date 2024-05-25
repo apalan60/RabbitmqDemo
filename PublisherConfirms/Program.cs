abeb06230a1c7d11db1ce449556b6aaf66a76040
apalan60 <apalan60@gmail.com>
2024-05-25 14:24:13 +0800

    PublishMessagesInBatch

--- PublisherConfirms/Program.cs
+++ PublisherConfirms/Program.cs
@@ -4,6 +4,9 @@ using RabbitMQ.Client;
 
 const int messageCount = 50000;
 
+PublishMessagesIndividually();
+PublishMessagesInBatch();
+
 static void PublishMessagesIndividually()
 {
     using var connection = CreateConnection();
@@ -25,6 +28,40 @@ static void PublishMessagesIndividually()
     Console.WriteLine($"Published {messageCount:N0} messages individually in {Stopwatch.GetElapsedTime(startTime, endTime).TotalMilliseconds:N0} ms");
 }
 
+static void PublishMessagesInBatch()
+{
+    using var connection = CreateConnection();
+    using var channel = connection.CreateModel();
+    
+    var queueName = channel.QueueDeclare().QueueName;
+    channel.ConfirmSelect();
+    
+    var batchSize = 1000;
+    var outstandingMessageCount = 0;
+    
+    var startTime = Stopwatch.GetTimestamp();
+    for (int i = 0; i < messageCount; i++)
+    {
+        var body = Encoding.UTF8.GetBytes(i.ToString());
+        channel.BasicPublish("", queueName, null, body);
+        outstandingMessageCount++;
+        
+        if (outstandingMessageCount == batchSize)
+        {
+            channel.WaitForConfirmsOrDie(TimeSpan.FromSeconds(5)); //Waits until all messages published on this channel since the last call have been ack by the broker. If a nack is received or the timeout
+            outstandingMessageCount = 0;
+        }
+    }
+
+    if (outstandingMessageCount > 0)
+    {
+        channel.WaitForConfirmsOrDie(TimeSpan.FromSeconds(5));
+    }
+    
+    var endTime = Stopwatch.GetTimestamp();
+    
+    Console.WriteLine($"Published {messageCount:N0} messages in batch of {batchSize} in {Stopwatch.GetElapsedTime(startTime, endTime).TotalMilliseconds:N0} ms");
+}
 
 
 static IConnection CreateConnection()
