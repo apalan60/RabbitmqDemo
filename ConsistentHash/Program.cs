648b0bc6ea09da010f49a82ce453099c344139d6
apalan60 <apalan60@gmail.com>
2024-05-28 08:35:50 +0800

    setup project 'ConsistentHash'

---
+++ ConsistentHash/Program.cs
@@ -0,0 +1,9 @@
+﻿using RabbitMQ.Client;
+
+var factory = new ConnectionFactory
+{
+    HostName = "127.0.0.1"
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
