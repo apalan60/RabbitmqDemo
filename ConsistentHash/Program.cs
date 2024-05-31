06525bbb2aea7f578e85e00e16867d59d7c0ebc8
apalan60 <apalan60@gmail.com>
2024-05-31 23:24:47 +0800

    declare consistent hash exchange, then publish messages

--- ConsistentHash/Program.cs
+++ ConsistentHash/Program.cs
@@ -9,16 +9,31 @@ var factory = new ConnectionFactory
 using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
 
+const string exchange = "ex.Hash";
+const string exchangeType = "x-consistent-hash";
+channel.ExchangeDeclare(exchange, exchangeType, durable: true, autoDelete: false);
+
+for (var i = 1; i < 4; i++)
+{
+    var queue = channel.QueueDeclare($"q.consistent.Hash.{i}", durable: true, exclusive: false, autoDelete: false).QueueName;
+    channel.QueuePurge(queue);
+
+    var routingKey = i.ToString();  //Binding Weight  => 1 :  2 :  3  
+    channel.QueueBind(queue, exchange, routingKey);
+    Console.WriteLine($"queue:{queue},  binding weight:{routingKey}");
+}
+
+
+
 channel.ConfirmSelect();
 
 var properties = channel.CreateBasicProperties();
-const string exchangeName = "ex.hash";
 
 //Publish 100k messages
 for (int i = 0; i < 100000; i++)
 {
     var body = Encoding.UTF8.GetBytes(i.ToString());
-    channel.BasicPublish(exchange: exchangeName,
+    channel.BasicPublish(exchange: exchange,
         routingKey: i.ToString(),   //different routing key for each message
         basicProperties: properties,
         body: body);
