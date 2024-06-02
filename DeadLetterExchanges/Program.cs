6f3e7cc4392fbffea8b60ddb95cdead4b2642cb5
apalan60 <apalan60@gmail.com>
2024-06-03 07:24:23 +0800

    add new solution folder for 'dead letter exchanges'

---
+++ DeadLetterExchanges/Program.cs
@@ -0,0 +1,13 @@
+﻿using RabbitMQ.Client;
+
+var factory = new ConnectionFactory{ HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "dead_letter_exchange", type: ExchangeType.Direct);
+
+var queue = channel.QueueDeclare(queue: "dead_letter_queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+channel.QueueBind(queue: queue.QueueName, exchange: "dead_letter_exchange", routingKey: string.Empty);
+
+Console.WriteLine(" [*] Waiting for dead letters.");
